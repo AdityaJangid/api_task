be90ab5ccc75f2a9062c22a0766eb5f28be2841f
AdityaJangid <eradityajangid@gmail.com>
2017-05-31 09:15:04 +0530

    task files

--- task.py
+++ task.py
@@ -3,6 +3,7 @@ import os
 import commands
 from flask import Flask, jsonify, abort, make_response, session, app
 from flask import Response
+import requests
 from flask import request
 import json
 
@@ -33,12 +34,48 @@ def get_data():
     return "{status:ok}"
 
 @app.route('/api/serverStatus', methods = ['GET'])
-def api_hello():
+def ServerStatus():
 
-    a = commands.getoutput("cat task.json")
-    print(a)
-    return a
+    with open("task.json", 'r+') as f:
+        dict1 = eval(f.read())
+    dict2 = str(dict1)
+    return dict2
 
+
+
+
+@app.route('/api/kil', methods = ['PUT'])
+def put():
+    conn_id = request.args.get('connid')
+    print(conn_id)
+    #  print(conn_id)
+    #  print(type(conn_id))
+    with open("task.json", 'r+') as f:
+        dict1 = eval(f.read())
+
+    print(dict1)
+    dict1.pop(conn_id)
+    print(dict1)
+    f = open("task.json", "w")
+    f.write(json.dumps(dict1, f))
+
+    with open("task.json", 'r+') as f:
+        dict1 = eval(f.read())
+    dict2 = str(dict1)
+    print( dict2)
+
+    #  return "{status:killed}"
+    return redirect(url_for('status'))
+    #  return (resp.text, resp.status_code, resp.headers.items())
+
+#  @app.route('/api/status', method=['GET'])
+#  def status(self):
+#      #  if self.conn_id
+#      return "hello world"
+
+
+
+   #  return "hello "
 
 
 
